d8bb34787f15f225da0b3ed2635bc0cb272d6e6c
skraazh <lukas.munozm@utem.cl>
2020-11-25 17:55:21 -0300

    ap con union y conca, aun por terminar

---
+++ automatapila/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata_pila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AP auto1 = new AP();
+
+
+            Console.Write("ingrese el num total de estados: ");
+            string v = Console.ReadLine();
+            int n = Convert.ToInt32(v);
+            auto1.setnumEstados(n);
+            Console.WriteLine();
+
+            auto1.setEstadoInicial(0);
+
+            Console.Write("ingrese el num del estados finales: ");
+            string f = Console.ReadLine();
+            int nf = Convert.ToInt32(f);
+            Console.WriteLine();
+
+            for(int i=0;i<nf;i++)
+            {
+                Console.Write("ingrese el numero del estado final: ");
+                string ef = Console.ReadLine();
+                int nef = Convert.ToInt32(ef);
+                auto1.addEstadoFinal(nef);
+                Console.WriteLine();
+            }
+
+            Console.Clear();
+
+            Console.Write("ingrese el num total del alfabeto: ");
+            string tr = Console.ReadLine();
+            int ntr = Convert.ToInt32(tr);
+            auto1.settotalTransiciones(ntr);
+            Console.WriteLine();
+
+            for (int i = 0; i < ntr; i++)
+            {
+                Console.WriteLine("ingrese la transicion: ");
+                string trs = Console.ReadLine();
+                auto1.addLetraAlfabeto(trs);
+                Console.WriteLine();
+            }
+
+            Console.Write("Cuantas transiciones piensa realizar: ");
+            string nt = Console.ReadLine();
+            int t = Convert.ToInt32(nt);
+
+            for (int i=0;i<t;i++)
+            {
+                Console.Write("la transicion empieza en: ");
+                string ei = Console.ReadLine();
+                int nei = Convert.ToInt32(ei);
+                Console.WriteLine();
+
+                Console.Write("la transicion se realiza con: ");
+                string transi = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("la transicion termina en: ");
+                string ef = Console.ReadLine();
+                int nef = Convert.ToInt32(ef);
+                Console.WriteLine();
+
+                auto1.addTransicion(nei, transi, nef);
+            }
+
+            Console.Clear();
+
+
+            for (int i=0;i<auto1.getnumEstados();i++ )
+            {
+                for(int j=0;j<auto1.gettotalTransiciones();j++)
+                {
+                    Console.Write("[");
+                    foreach (int tra in auto1.getTablaTransiciones()[i, j])
+                    {
+                        Console.Write(tra);
+                    }
+                    Console.Write("]");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ automatapila/concatenacion.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata_pila
+{
+    class concatenacion
+    {
+        int numEstados;
+        int estadoInicial;
+        int totalTransiciones;
+        SortedSet<string> alfabeto;
+        SortedSet<int> estadosFinales;
+        SortedSet<int>[,] tablaTransiciones;
+
+        public AP concatenar(AP ap1, AP ap2)
+        {
+            numEstados = ap1.getnumEstados() + ap2.getnumEstados() + 2;
+            estadoInicial = 0;
+            totalTransiciones = 2 + ap1.gettotalTransiciones() + ap2.gettotalTransiciones();
+
+            alfabeto.Add("@/@/Ind");
+            alfabeto.Add("@/Ind/@");
+            alfabeto.Add("@/@/@");
+            foreach( string alf1 in ap1.getAlfabeto())
+            {
+                alfabeto.Add(alf1);
+            }
+
+            foreach (string alf2 in ap2.getAlfabeto())
+            {
+                alfabeto.Add(alf2);
+            }
+
+            foreach (int eFinal in ap2.getestadoFinal())
+            {
+                estadosFinales.Add(eFinal+2+ap1.getnumEstados());
+            }
+
+            SortedSet<int>[,] tabUnion = new SortedSet<int>[numEstados,totalTransiciones];
+            for (int x = 0; x < numEstados; x++)
+            {
+                for (int y = 0; y < totalTransiciones; y++)
+                {
+                    tabUnion[x, y] = new SortedSet<int>();
+                }
+            }
+
+            int cont1 = 0;
+            int dif1 = 0;
+
+            foreach (string trn in alfabeto)
+            {
+                if (trn != "@/@/Ind" && dif1 == 0)
+                    cont1++;
+                if (trn == "@/@/Ind")
+                    dif1++;
+            }
+            tabUnion[0, cont1].Add(ap1.getEstadoInicial()+1);
+
+            int cont2 = 0;
+            int dif2 = 0;
+            foreach (string trn in alfabeto)
+            {
+                if (trn != "@/Ind/@" && dif2 == 0)
+                    cont2++;
+                if (trn == "@/Ind/@")
+                    dif2++;
+            }
+            tabUnion[(ap1.getnumEstados()),cont2].Add(1+ap1.getnumEstados());
+
+            int cont3 = 0;
+            int dif3 = 0;
+            foreach (string trn in alfabeto)
+            {
+                if (trn != "@/@/@" && dif2 == 0)
+                    cont3++;
+                if (trn == "@/@/@")
+                    dif3++;
+            }
+            tabUnion[(2+ap1.getnumEstados()), cont3].Add(ap2.getEstadoInicial() + ap1.getnumEstados()+2);
+
+            return new AP(numEstados, estadoInicial,totalTransiciones,alfabeto,estadosFinales,tabUnion);
+        }
+    }
+}
